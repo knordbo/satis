4070d1887bcb8840857bfa20138be7f3c27ad50e
Kjartan Nordbo <kjartan.kilen.nordbo@gmail.com>
2018-10-01 21:14:41 +1000

    material theme

--- app/src/main/java/com/satis/app/MainActivity.kt
+++ app/src/main/java/com/satis/app/MainActivity.kt
@@ -3,12 +3,15 @@ package com.satis.app
 import android.os.Bundle
 import com.airbnb.mvrx.BaseMvRxActivity
 import com.satis.app.feature.cards.CardFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseMvRxActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
 
         supportFragmentManager.beginTransaction()
                 .replace(R.id.fragmentContainer, CardFragment())
