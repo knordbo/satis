6b74d5b769f3c140e30e237477db05711007d090
Kjartan Nordbo <kjartan.kilen.nordbo@gmail.com>
2018-10-14 22:04:49 +1100

    cleanup

--- app/src/main/java/com/satis/app/MainActivity.kt
+++ app/src/main/java/com/satis/app/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.satis.app
 
 import android.os.Bundle
-import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -10,14 +9,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseMvRxActivity() {
 
-    private val navigationController: NavController
-        get() = findNavController(R.id.navigationHostFragment)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(toolbar)
+
+        val navigationController = findNavController(R.id.navigationHostFragment)
 
         bottomNav.setupWithNavController(navigationController)
         setupActionBarWithNavController(navigationController)
