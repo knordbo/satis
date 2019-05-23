3711e72fde73bf1154009c65871ab9decc0b0529
Kyle Nordbo <kjartan.kilen.nordbo@gmail.com>
2019-05-24 09:56:58 +1000

    auto battery

--- app/src/main/java/com/satis/app/common/prefs/Theme.kt
+++ app/src/main/java/com/satis/app/common/prefs/Theme.kt
@@ -1,15 +1,23 @@
 package com.satis.app.common.prefs
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
-import androidx.appcompat.app.AppCompatDelegate.NightMode
+import com.satis.app.common.prefs.Theme.DARK
+import com.satis.app.common.prefs.Theme.LIGHT
+import com.satis.app.common.prefs.Theme.SYSTEM
 
-enum class Theme(val themeName: String, @NightMode val nightMode: Int) {
-    SYSTEM("system", MODE_NIGHT_FOLLOW_SYSTEM),
-    LIGHT("light", MODE_NIGHT_NO),
-    DARK("dark", MODE_NIGHT_YES)
+enum class Theme(val themeName: String) {
+    SYSTEM("system"),
+    LIGHT("light"),
+    DARK("dark")
 }
 
-fun Theme.apply() = AppCompatDelegate.setDefaultNightMode(nightMode)
+fun Theme.apply() = AppCompatDelegate.setDefaultNightMode(when (this) {
+    SYSTEM -> if (Build.VERSION.SDK_INT > 28) MODE_NIGHT_FOLLOW_SYSTEM else MODE_NIGHT_AUTO_BATTERY
+    LIGHT -> MODE_NIGHT_NO
+    DARK -> MODE_NIGHT_YES
+})
