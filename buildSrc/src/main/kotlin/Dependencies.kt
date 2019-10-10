7755846f6356d61573c379607ec7e6153142e102
knordbo <kjartan.kilen.nordbo@gmail.com>
2019-10-10 14:21:11 +1100

    bump deps

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -5,15 +5,15 @@ private object Versions {
         const val appCompat = "1.1.0"
         const val cardView = "1.0.0"
         const val constraintLayout = "2.0.0-beta2" // TODO when bumping version delete the attrs.xml workaround
-        const val coreKtx = "1.2.0-alpha04"
+        const val coreKtx = "1.2.0-beta01"
         const val coreTesting = "2.1.0"
         const val espresso = "3.3.0-alpha02"
-        const val fragment = "1.2.0-alpha04"
-        const val concurrent = "1.0.0-rc01"
-        const val lifecycle = "2.2.0-alpha05"
-        const val navigation = "2.2.0-alpha03"
-        const val recyclerView = "1.1.0-beta04"
-        const val room = "2.2.0-rc01"
+        const val fragment = "1.2.0-beta01"
+        const val concurrent = "1.0.0"
+        const val lifecycle = "2.2.0-beta01"
+        const val navigation = "2.2.0-beta01"
+        const val recyclerView = "1.1.0-beta05"
+        const val room = "2.2.0"
         const val test = "1.3.0-alpha02"
         const val testExt = "1.1.2-alpha02"
     }
@@ -54,7 +54,7 @@ private object Versions {
 
     const val tinydancer = "0.1.2"
     const val stetho = "1.5.1"
-    const val workManager = "2.3.0-alpha01"
+    const val workManager = "2.3.0-alpha02"
 }
 
 object Deps {
