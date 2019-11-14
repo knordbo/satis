bc7c0d8504200acb23be5debfa537a4b26fa2f44
knordbo <kjartan.kilen.nordbo@gmail.com>
2019-11-14 17:23:54 +0100

    try gradle 6.0 again

--- buildSrc/src/main/kotlin/BuildVersions.kt
+++ buildSrc/src/main/kotlin/BuildVersions.kt
@@ -1,4 +1,5 @@
 object BuildVersions {
     const val minSdk = 24
     const val targetSdk = 29
+    const val ndk = "20.0.5594570" // TODO remove when newer supported by github https://help.github.com/en/actions/automating-your-workflow-with-github-actions/software-installed-on-github-hosted-runners
 }
