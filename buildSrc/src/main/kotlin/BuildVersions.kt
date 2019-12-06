ed23e2c5ab0e20b7818a77f8bf70aba7b856cca3
knordbo <kjartan.kilen.nordbo@gmail.com>
2019-12-06 02:09:58 -0500

    update ndk version

--- buildSrc/src/main/kotlin/BuildVersions.kt
+++ buildSrc/src/main/kotlin/BuildVersions.kt
@@ -1,5 +1,5 @@
 object BuildVersions {
     const val minSdk = 24
     const val targetSdk = 29
-    const val ndk = "20.0.5594570" // TODO remove when https://issuetracker.google.com/issues/143630825 is fixed
+    const val ndk = "20.1.5948944" // TODO remove when https://issuetracker.google.com/issues/143630825 is fixed
 }
