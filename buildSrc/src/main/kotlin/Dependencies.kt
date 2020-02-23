1be48ee3feff79233ec01b7c6b18428e0ca725b1
knordbo <kjartan.kilen.nordbo@gmail.com>
2020-02-23 18:08:55 -0500

    bump deps

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -7,15 +7,15 @@ private object Versions {
         const val constraintLayout = "2.0.0-beta4"
         const val coreKtx = "1.3.0-alpha01"
         const val coreTesting = "2.1.0"
-        const val espresso = "3.3.0-alpha03"
-        const val fragment = "1.2.1"
+        const val espresso = "3.3.0-alpha04"
+        const val fragment = "1.2.2"
         const val concurrent = "1.1.0-alpha01"
         const val lifecycle = "2.2.0"
-        const val navigation = "2.3.0-alpha01"
+        const val navigation = "2.3.0-alpha02"
         const val recyclerView = "1.2.0-alpha01"
-        const val test = "1.3.0-alpha03"
-        const val testExt = "1.1.2-alpha03"
-        const val workManager = "2.3.1"
+        const val test = "1.3.0-alpha04"
+        const val testExt = "1.1.2-alpha04"
+        const val workManager = "2.3.2"
     }
 
     const val crashlytics = "2.10.1"
@@ -28,7 +28,7 @@ private object Versions {
     const val fabric = "1.29.0"
     const val firebaseCore = "17.2.2"
     const val firebaseFirestore = "21.4.0"
-    const val flipper = "0.30.2"
+    const val flipper = "0.31.2"
     const val glide = "4.11.0"
     const val googlePlayPublisher = "2.6.1"
     const val googleServices = "4.0.0"
@@ -42,18 +42,18 @@ private object Versions {
     }
 
     const val leakCanary = "2.2"
-    const val material = "1.2.0-alpha04"
+    const val material = "1.2.0-alpha05"
     const val mvrx = "2.0.0-alpha2"
-    const val okhttp = "4.3.1"
+    const val okhttp = "4.4.0"
     const val photoView = "2.3.0"
-    const val playCore = "1.6.4"
+    const val playCore = "1.6.5"
 
     object retrofit {
         const val kotlinSerialization = "0.4.0"
         const val retrofit = "2.7.1"
     }
 
-    const val soloader = "0.8.1"
+    const val soloader = "0.8.2"
     const val sqldelight = "1.2.2"
 }
 
