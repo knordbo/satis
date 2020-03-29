892a0c302fa46f0539c5c2975b0ab96b7648d25b
knordbo <kjartan.kilen.nordbo@gmail.com>
2020-03-29 13:29:16 -0400

    bump minsdk

--- buildSrc/src/main/kotlin/BuildVersions.kt
+++ buildSrc/src/main/kotlin/BuildVersions.kt
@@ -1,5 +1,5 @@
 object BuildVersions {
-  const val minSdk = 26
+  const val minSdk = 28
   const val targetSdk = 29
   const val ndk = "21.0.6113669" // TODO remove when https://issuetracker.google.com/issues/143630825 is fixed
 }
