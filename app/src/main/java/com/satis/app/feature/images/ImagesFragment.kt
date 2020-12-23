8751a85803bc1133278ef4f6f5e336db885e4b1b
Kjartan Nordbo <kjartan.kilen.nordbo@gmail.com>
2020-12-23 13:59:24 -0800

    images compose

--- app/src/main/java/com/satis/app/feature/images/ImagesFragment.kt
+++ app/src/main/java/com/satis/app/feature/images/ImagesFragment.kt
@@ -4,63 +4,69 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.GridLayoutManager
 import com.airbnb.mvrx.fragmentViewModel
-import com.airbnb.mvrx.withState
-import com.satis.app.R
-import com.satis.app.common.fragment.BaseViewBindingFragment
-import com.satis.app.common.navigation.NavigationReselection
-import com.satis.app.databinding.FeatureImagesBinding
-import com.satis.app.feature.images.ui.ImagesAdapter
-import com.satis.app.utils.context.requireDrawable
-import com.satis.app.utils.view.disableChangeAnimations
+import com.satis.app.common.fragment.BaseFragment
+import com.satis.app.common.theme.AppTheme
+import dev.chrisbanes.accompanist.coil.CoilImage
 import javax.inject.Inject
 
 class ImagesFragment @Inject constructor(
   private val viewModelFactory: ImagesViewModel.Factory,
-  private val navigationReselection: NavigationReselection
-) : BaseViewBindingFragment<FeatureImagesBinding>(), ImagesViewModel.Factory by viewModelFactory {
+) : BaseFragment(), ImagesViewModel.Factory by viewModelFactory {
 
   private val imagesViewModel: ImagesViewModel by fragmentViewModel()
-  private val adapter by lazy {
-    ImagesAdapter(
-      imageClicked = { photo ->
-        findNavController().navigate(ImagesFragmentDirections.actionImagesToImage(photo, photo.description.orEmpty()))
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    return ComposeView(inflater.context).apply {
+      layoutParams = ViewGroup.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+      setContent {
+        AppTheme {
+          val state = imagesViewModel.stateFlow.collectAsState(ImagesState())
+          LazyColumn {
+            val items = state.value.photoState.toPhotoStateItems()
+            items(items) { rows ->
+              Row(
+                modifier = Modifier.padding(bottom = (if (rows === items.lastOrNull()) 0 else 2).dp)
+              ) {
+                rows.forEach { item ->
+                  if (item.row > 0) {
+                    Spacer(modifier = Modifier.width(2.dp))
+                  }
+                  CoilImage(
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                      .aspectRatio(1f)
+                      .weight(1f)
+                      .clickable {
+                        findNavController().navigate(ImagesFragmentDirections.actionImagesToImage(item.photoState, item.photoState.description.orEmpty()))
+                      },
+                    data = item.photoState.photoUrl,
+                  )
+                }
+              }
+            }
+          }
+        }
       }
-    )
-  }
-
-  override val bind: (LayoutInflater, ViewGroup?, Boolean) -> FeatureImagesBinding? =
-    FeatureImagesBinding::inflate
-
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-
-    binding.images.adapter = adapter
-    binding.images.layoutManager = GridLayoutManager(requireContext(), COLUMNS)
-    binding.images.disableChangeAnimations()
-
-    val dividerDrawable = requireContext().requireDrawable(R.drawable.divider)
-    binding.images.addItemDecoration(GridDividerItemDecoration(dividerDrawable, dividerDrawable, COLUMNS))
-
-    navigationReselection.addReselectionListener(viewLifecycleOwner, R.id.images) {
-      binding.images.smoothScrollToPosition(0)
-      imagesViewModel.onReselected()
-    }
-  }
-
-  override fun onDestroyView() {
-    binding.images.adapter = null
-    super.onDestroyView()
-  }
-
-  override fun invalidate() {
-    withState(imagesViewModel) { imageState ->
-      adapter.submitList(imageState.photoState)
     }
   }
 
 }
+
+private fun List<PhotoState>.toPhotoStateItems(): List<List<PhotoStateItem>> = mapIndexed { index, photoState ->
+  PhotoStateItem(photoState = photoState, row = index % COLUMNS, column = index / COLUMNS)
+}.groupBy { it.column }.map { it.value }.toList()
+
+data class PhotoStateItem(val photoState: PhotoState, val row: Int, val column: Int)
 
 private const val COLUMNS = 3
