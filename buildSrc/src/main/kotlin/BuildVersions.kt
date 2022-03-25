4eff2caba24ede7c8f61aace2573689be49d04ef
knordbo <kjartan.kilen.nordbo@gmail.com>
2022-03-25 19:58:32 -0400

    update target sdk

--- buildSrc/src/main/kotlin/BuildVersions.kt
+++ buildSrc/src/main/kotlin/BuildVersions.kt
@@ -1,6 +1,6 @@
 object BuildVersions {
   const val minSdk = 28
-  const val targetSdk = 31
+  const val targetSdk = 32
   const val compileSdk = "Tiramisu"
   const val ndk = "18.1.5063045" // TODO remove when https://issuetracker.google.com/issues/143630825 is fixed
 }
