6f11a100f706ecd4b39feae485c76ab4a52c94da
knordbo <kjartan.kilen.nordbo@gmail.com>
2022-03-27 17:58:33 -0400

    annotate

--- app/src/main/java/com/satis/app/utils/context/ContextHolder.kt
+++ app/src/main/java/com/satis/app/utils/context/ContextHolder.kt
@@ -1,7 +1,9 @@
 package com.satis.app.utils.context
 
+import android.annotation.SuppressLint
 import android.content.Context
 
+@SuppressLint("StaticFieldLeak")
 object ContextHolder {
   lateinit var context: Context
 }
