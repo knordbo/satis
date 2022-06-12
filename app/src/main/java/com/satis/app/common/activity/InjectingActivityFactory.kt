08b284b4b375717f25959bf4f2cdb21c6e5eabcd
knordbo <kjartan.kilen.nordbo@gmail.com>
2022-06-12 13:36:13 -0400

    remove activity injector

--- app/src/main/java/com/satis/app/common/activity/InjectingActivityFactory.kt
+++
@@ -1,8 +0,0 @@
-package com.satis.app.common.activity
-
-import android.app.Activity
-import android.content.Intent
-
-interface InjectingActivityFactory {
-  fun instantiateActivityCompat(cl: ClassLoader, className: String, intent: Intent?): Activity
-}
